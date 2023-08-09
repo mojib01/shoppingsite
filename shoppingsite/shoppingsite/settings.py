487e7f1cb07b4d52e2dab4a4d3a92dcc6111df0a
mojib01 <mdmojibddin@gmail.com>
2023-08-09 13:55:34 +0530

    'deploying'

--- shoppingsite/shoppingsite/settings.py
+++ shoppingsite/shoppingsite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['vercel.app', 'now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
